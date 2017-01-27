a173999966419ab56d91c036ca75cb7a9d818772
Megan Reardon <meganr@gmail.com>
2017-01-27 09:50:41 -0800

    added index entry and config files

--- lab-megan/app/config/log-config.js
+++ lab-megan/app/config/log-config.js
@@ -3,5 +3,5 @@
 module.exports = ['$logProvider', logConfig];
 
 function logConfig($logProvider) {
-  $logProvider.debugEnabled(__DEBUG__); // turn on or off // eslint.diable.line
+  $logProvider.debugEnabled(__DEBUG__);
 }
