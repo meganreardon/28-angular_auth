59f8ddaee0ca097d7775e327c02153c97cc1f4e4
Megan Reardon <meganr@gmail.com>
2017-02-02 15:59:14 -0800

    have fetch galleries test working

---
+++ lab-megan/test/auth-service-test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('Auth Service', function() {
+
+  beforeEach( () => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(($rootScope, authService, $window, $httpBackend) => {
+      this.$window = $window;
+      this.$rootScope = $rootScope;
+      this.authService = authService;
+      this.$httpBackend = $httpBackend;
+    });
+  });
+
+  describe('authService.getToken()', () => {
+    it('should return a token', () => {
+      this.authService.token = null;
+      this.$window.localStorage.setItem('token', 'test token');
+
+      this.authService.getToken()
+      .then( token => {
+        expect(token).toEqual('test token');
+      })
+      .catch( err => { // we don't need this catch
+        expect(err).toEqual(null);
+      });
+
+      this.$rootScope.$apply();  // for now throw this at bottom of test pages
+    });
+  });
+
+});
