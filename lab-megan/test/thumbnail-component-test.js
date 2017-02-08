3ffea376ecc06e892074e78d0f98d27e711a0105
Megan Reardon <meganr@gmail.com>
2017-02-07 21:44:46 -0800

    cleaned code

--- lab-megan/test/thumbnail-component-test.js
+++ lab-megan/test/thumbnail-component-test.js
@@ -12,25 +12,6 @@ describe('Thumbnail Component', function() {
     });
   });
 
-// below is from other test
-  // describe('galleryService.deleteGallery()', () => {
-  //   it('should delete a gallery', () => {
-  //     let galleryID = 'testid';
-  //     let headers = {
-  //       Authorization: 'Bearer test token',
-  //       Accept: 'application/json, text/plain, */*'
-  //     };
-  //
-  //     this.$httpBackend.expectDELETE('http://localhost:8000/api/gallery/testid', headers)
-  //     .respond(204);
-  //
-  //     this.galleryService.deleteGallery(galleryID);
-  //     this.$httpBackend.flush();
-  //     this.$rootScope.$apply();
-  //   });
-  // });
-// end from another page
-
   describe('thumbnail component test, delete pic', () => {
     it('should delete a photograph', () => {
 
