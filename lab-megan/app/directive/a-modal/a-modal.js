bec9c6bf9bb6c1f60d3d8424bd2e5ff07eb3cf48
Megan Reardon <meganr@gmail.com>
2017-02-09 10:26:39 -0800

    modal button click registering

--- lab-megan/app/directive/a-modal/a-modal.js
+++ lab-megan/app/directive/a-modal/a-modal.js
@@ -6,15 +6,66 @@ module.exports = function() {
   return {
     restrict: 'EAC',
     template: require('./a-modal.html'),
-    controller: ['$log', AModalController],
+    controller: ['$log', '$uibModal', AModalController],
     bindToController: true,
     controllerAs: 'aModalCtrl',
-    scopt: {
+    scope: {
       title: '@'
     }
   };
 };
 
-function AModalController() {
+function AModalController($log, $uibModal) {
+  $log.debug('AModalController');
+
   this.stuffInModal = ['Congratulations! You\'ve opened a modal window! Also known as: The Please Sign Up For Our Mailing List Annoyance Window.'];
+
+  this.openModal = function() {
+    $log.log('\n\n::: openModal() :::');
+    $uibModal.open();
+  };
+
+  this.closeModal = function() {
+    $uibModal.close();
+  };
 }
+
+// from ex
+// app.controller('SampleController', function($scope, ModalService) {
+//
+//   ModalService.showModal({
+//     templateUrl: "template.html",
+//     controller: "ModalController"
+//   }).then(function(modal) {
+//
+//     //it's a bootstrap element, use 'modal' to show it
+//     modal.element.modal();
+//     modal.close.then(function(result) {
+//       console.log(result);
+//     });
+//   });
+//
+// );
+
+// borrowed code
+// require('./_create-gallery.scss');
+//
+// module.exports = {
+//   template: require('./create-gallery.html'),
+//   controller: ['$log', 'galleryService', CreateGalleryController],
+//   controllerAs: 'createGalleryCtrl'
+// };
+//
+// function CreateGalleryController($log, galleryService) {
+//   $log.debug('CreateGalleryController');
+//
+//   this.gallery = {};
+//
+//   this.createGallery = function() {
+//     galleryService.createGallery(this.gallery)
+//     .then( () => {
+//       this.gallery.name = null;
+//       this.gallery.desc = null;
+//     });
+//   };
+// }
